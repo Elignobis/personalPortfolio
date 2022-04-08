ebd1f3b6887478ac8fff9b8385878918b49a6069
Elignobis <rwayisibongile@gmail.com>
2022-04-08 14:38:19 +0200

    friday 14:38 08/04/22 commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Tyiping animation */
+
+var typed = new Typed(".typing", {
+    strings:["Full-stack Web Developer", "Web Designer", "Lover of everything tech!"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop:true
+});
